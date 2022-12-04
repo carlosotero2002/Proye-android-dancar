c0d3b8d88407f7da3a11eff7e721b10e04bb39e4
daniel117622 <dan5jul@hotmail.com>
2022-12-03 21:11:42 -0600

    go method

--- MyBank/app/src/main/java/com/example/mybank/MainActivity.java
+++ MyBank/app/src/main/java/com/example/mybank/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.mybank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void go(View view)
+    {
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
     }
 }
